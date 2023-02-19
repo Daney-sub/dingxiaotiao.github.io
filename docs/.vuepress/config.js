fa6fb72f8be9cdbc6d797cdd7b7ea6357f5e8cff
dingxiaotiao <75723798+dingxiaotiao@users.noreply.github.com>
2023-02-20 00:37:11 +0800

    1

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,8 +3,18 @@ import { defaultTheme } from '@vuepress/theme-default'
 
 export default {
     base:"/",
+    
     theme: defaultTheme({
       // 默认主题配置
+      Array<{
+        text: string
+        link: string
+        type?: 'primary' | 'secondary'
+      }>
+      Array<{
+        title: string
+        details: string
+      }>
       navbar: [
         {
           text: '首页',
