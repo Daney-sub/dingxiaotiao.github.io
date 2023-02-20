697e162b116edfce79118d608a2405bfa7c51d23
dingxiaotiao <75723798+dingxiaotiao@users.noreply.github.com>
2023-02-20 12:09:09 +0800

    更新 config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,19 +2,22 @@ import { defaultTheme } from '@vuepress/theme-default'
 
 
 export default {
-    base:"/",
-    head: [['link', { rel: 'icon', href: '/public/logo.png' }]],
-    theme: defaultTheme({
-      // 默认主题配置
-      navbar: [
-        {
-          text: '首页',
-          link: '/',
-        },
-        {
-          text: '新手上路',
-          link: '/guide/getting-started',
-        }
-      ],
-    }),
-  }
+  base: "/",
+  head: [['link', { rel: 'icon', href: '/logo.png' }]],
+  lang: 'zh-cn',
+  title: '叮小跳',
+  description: '一款强大的跳过广告应用',
+  theme: defaultTheme({
+    // 默认主题配置
+    navbar: [
+      {
+        text: '首页',
+        link: '/',
+      },
+      {
+        text: '新手上路',
+        link: '/guide/getting-started',
+      }
+    ],
+  }),
+}
