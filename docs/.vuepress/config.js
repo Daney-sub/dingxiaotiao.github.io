a558665653bb896c5a381661c0517654070305c7
dingxiaotiao <75723798+dingxiaotiao@users.noreply.github.com>
2023-02-20 12:38:25 +0800

    更新 config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -19,20 +19,5 @@ export default {
         link: '/guide/getting-started',
       }
     ],
-    // 侧边栏对象
-    // 不同子路径下的页面会使用不同的侧边栏
-    sidebar: {
-      '/guide/': [
-        {
-          text: 'Guide',
-          children: ['/guide/README.md', '/guide/getting-started.md'],
-        },
-      ],
-      '/reference/': [
-        {
-          text: 'Reference',
-          children: ['/reference/cli.md', '/reference/config.md'],
-        },
-      ],
 
   }),
